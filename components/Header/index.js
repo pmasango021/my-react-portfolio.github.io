feb58ee52807a9178b35c95e43d32864d38d482b
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-10-31 21:19:19 +0200

    Email Buttons Update

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:mailto:phillipsm@myself.com")
                       }
                     >
                       Resume
@@ -85,7 +85,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:mailto:phillipsm@myself.com")}
                   >
                     Contact
                   </Button>
@@ -108,7 +108,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:mailto:phillipsm@myself.com")}
                   >
                     Contact
                   </Button>
@@ -131,11 +131,11 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
         </h1>
         {!isBlog ? (
           <div className="flex">
-            <Button onClick={handleWorkScroll}>Work</Button>
+            {/*<Button onClick={handleWorkScroll}>Work</Button>*/}
             <Button onClick={handleAboutScroll}>About</Button>
-            {showBlog && (
-              <Button onClick={() => router.push("/blog")}>Blog</Button>
-            )}
+            {/*{showBlog && (*/}
+            {/*  <Button onClick={() => router.push("/blog")}>Blog</Button>*/}
+            {/*)}*/}
             {showResume && (
               <Button
                 onClick={() => router.push("/resume")}
@@ -145,7 +145,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:mailto:phillipsm@myself.com")}>
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -162,9 +162,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
         ) : (
           <div className="flex">
             <Button onClick={() => router.push("/")}>Home</Button>
-            {showBlog && (
-              <Button onClick={() => router.push("/blog")}>Blog</Button>
-            )}
+            {/*{showBlog && (*/}
+            {/*  // <Button onClick={() => router.push("/blog")}>Blog</Button>*/}
+            {/*)}*/}
             {showResume && (
               <Button
                 onClick={() => router.push("/resume")}
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:mailto:phillipsm@myself.com")}>
               Contact
             </Button>
 
--- pages/index.js
+++ pages/index.js
@@ -125,6 +125,19 @@ export default function Home() {
               />
             ))}
           </div>
+
+          {/*<div className="mt-10 laptop:mt-30 p-2 laptop:p-0">*/}
+          {/*  <h1 className="tablet:m-10 text-2xl text-bold">Tech Stacks & Frameworks.</h1>*/}
+          {/*  <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">*/}
+          {/*    {data.services.map((service, index) => (*/}
+          {/*        <ServiceCard*/}
+          {/*            key={index}*/}
+          {/*            name={service.title}*/}
+          {/*            description={service.description}*/}
+          {/*        />*/}
+          {/*    ))}*/}
+          {/*  </div>*/}
+
         </div>
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
           <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
