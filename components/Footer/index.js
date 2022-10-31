92cd8dc3f5c5f5436422a357a512611d7381b0a2
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-11-01 00:33:00 +0200

    Updated Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,7 +25,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With REACT, Next.js and Tailwind CSS by Phillip Sebole Masango
+        Made With REACT, Next.js and Tailwind CSS by Phillip Sebole Masango. Deployed with Vercel.
       </h1>
     </>
   );
